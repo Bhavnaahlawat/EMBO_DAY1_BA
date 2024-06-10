2b6c55f2471cd62af1eb8aeabf07e22030a757bd
Bhavnahlawat <ahlawatbhavna58@gmail.com>
2024-06-10 18:21:23 +0200

    trial

--- EMBODAY1TRY.R
+++ EMBODAY1TRY.R
@@ -6,3 +6,11 @@ mean(x,y)
 #trial 123
 
 test2s
+test3
+
+install.packages("usethis") # Latest version
+library(usethis)
+usethis::use_git_config(user.name = "Bhavnahlawat", user.email = "ahlawatbhavna58@gmail.com")
+
+test2
+
